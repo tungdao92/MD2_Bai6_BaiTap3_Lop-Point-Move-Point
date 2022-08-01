4ede9ee8875088862fec672aaa01d233408cd6da
unknown <tungdao923@gmail.com>
2022-08-01 15:40:45 +0700

    Move point

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(5,5);
+        System.out.println(point);
+        MovablePoint movablePoint = new MovablePoint(point.getX(), point.getY(), 10,20);
+        System.out.println("Trước khi move" + movablePoint);
+        movablePoint.move();
+        System.out.println("sau khi move" + movablePoint);
+
+    }
+}
